6a6b8dfacb7ba2371bb6606dc40660cb25581f06
Abdullah Shoaib <masterabdullah95@gmail.com>
2025-01-27 10:53:23 +0500

    langgraph chapter 2

--- langgraph/chapter2/main.py
+++ langgraph/chapter2/main.py
@@ -1,15 +1,20 @@
 from langgraph.graph import StateGraph, START, END
 
 from classes import State
-from nodes import node_1
+from nodes import node_1, node_2, node_3, conditional_node
 
 graph_builder = StateGraph(State)
 
 graph_builder.add_node("node_one", node_1)
+graph_builder.add_node("node_two", node_2)
+graph_builder.add_node("node_three", node_3)
+graph_builder.add_node("conditional_node", conditional_node)
 
 graph_builder.add_edge(START, "node_one")
-graph_builder.add_edge("node_one", END)
+graph_builder.add_conditional_edges("node_one", conditional_node)
+graph_builder.add_edge("node_two", END)
+graph_builder.add_edge("node_three", END)
 graph = graph_builder.compile()
 
-res = graph.invoke({"prompt": "Who is the founder of Paksitan?"})
+res = graph.invoke({"prompt": "Node flow tracking ..."})
 print(res)
--- langgraph/chapter2/nodes.py
+++ langgraph/chapter2/nodes.py
@@ -1,6 +1,7 @@
 from classes import State
 from langchain_google_genai import ChatGoogleGenerativeAI
 from decouple import config
+from random import randint
 
 GOOGLE_GEMINI_API_KEY = config('GOOGLE_GEMINI_API_KEY')
 
@@ -10,5 +11,14 @@ llm: ChatGoogleGenerativeAI = ChatGoogleGenerativeAI(
 )
 
 def node_1(state: State) -> str:
-    result = llm.invoke(state["prompt"])
-    return {"output": result.content}
+    return {"output": "node1 calling"}
+def node_2(state: State) -> str:
+    return {"output": "node2 calling"}
+def node_3(state: State) -> str:
+    return {"output": "node3 calling"}
+def conditional_node(state: State) -> str:
+    guess = randint(1, 2)
+    if guess == 1:
+        return "node_two"
+    else:
+        return "node_three"
